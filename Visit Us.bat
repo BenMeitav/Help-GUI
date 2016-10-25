3642e13d31025808ca2bd390c15c6d5d9914553a
BenMeitav <benmeitav@gmail.com>
2016-10-25 15:41:22 +0300

    Update Visit Us.bat

--- Visit Us.bat
+++ Visit Us.bat
@@ -1,7 +1,1 @@
-@echo off
-start https://github.com/HeyThereBenHere/Help-GUI
-start https://www.youtube.com/channel/UCOHGFfLSKtVHDPawf1ZZL5g
-start http://www.heytherebenhere.tk
-echo Thanks For Downloading Help-GUI %USERNAME%
-pause
-exit
+mkdir users
